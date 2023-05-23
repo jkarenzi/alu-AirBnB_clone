2ac510ce23f049c288846675be311166dc3443b6
jkarenzi <j.karenzi@alustudent.com>
2023-05-23 21:46:02 +0200

    changes

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -14,23 +14,31 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):
-        self.storage = FileStorage()
+        self.storage_1 = FileStorage()
 
     def test_all(self):
-        self.assertIsInstance(self.storage.all(), dict)
+        self.assertIsInstance(self.storage_1.all(), dict)
         
     def test_new(self):
         obj = State()
-        all_objects = self.storage.all()
+        all_objects = self.storage_1.all()
         self.assertIn("State.{}".format(obj.id), all_objects)
 
-    def test_save_and_reload(self):
+    def test_save(self):
         obj = City()
-        self.storage.new(obj)
-        self.storage.save()
+        self.storage_1.new(obj)
+        self.storage_1.save()
         with open('file.json', 'r') as file:
             data = json.load(file)
             self.assertIn("City.{}".format(obj.id), data)
 
+    def test_reload(self):  
+        obj = City()
+        self.storage_1.new(obj)
+        self.storage_1.save()
+        self.storage_1.reload()     
+        objs = FileStorage.__objects
+        self.assertIn("City.{}".format(obj.id), objs)
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()    
