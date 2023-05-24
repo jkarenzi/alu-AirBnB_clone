d785b63c3bc3ea61c7d5e694bec0f96090ec6fca
jkarenzi <j.karenzi@alustudent.com>
2023-05-24 13:46:31 +0200

    PEPE 8 changes

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -51,7 +51,8 @@ class TestBaseModel(unittest.TestCase):
     def test__str__(self):
         """test for __str__ of the BaseModel class"""
         actual_output = str(self.base_model)
-        self.assertEqual(actual_output,
+        self.assertEqual(
+            actual_output,
             f"[BaseModel] ({self.base_model.id}) {self.base_model.__dict__}")
 
 if __name__ == '__main__':
