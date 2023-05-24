32cf50102ec0e33037da0bcdec5b3f5dfc67dd40
jkarenzi <j.karenzi@alustudent.com>
2023-05-24 15:36:21 +0200

    changes

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """test for the FileStorage class"""
 
-import models
-import os
 import unittest
 import json
 from models.base_model import BaseModel
@@ -16,93 +14,31 @@ from models.engine.file_storage import FileStorage
 
 class TestFileStorage(unittest.TestCase):
     def setUp(self):
-        try:
-            os.rename("file.json", "tmp")
-        except IOError:
-            pass
-
-    def tearDown(self):
-        try:
-            os.remove("file.json")
-        except IOError:
-            pass
-        try:
-            os.rename("tmp", "file.json")
-        except IOError:
-            pass
-        FileStorage._FileStorage__objects = {}    
+        self.storage_1 = FileStorage()
 
     def test_all(self):
-        self.assertIsInstance(models.storage.all(), dict)
+        self.assertIsInstance(self.storage_1.all(), dict)
         
     def test_new(self):
         obj = State()
-        all_objects = models.storage.all()
+        all_objects = self.storage_1.all()
         self.assertIn("State.{}".format(obj.id), all_objects)
 
     def test_save(self):
-        obj1= BaseModel()
-        obj2 = User()
-        obj3 = State()
-        obj4 = Place()
-        obj5 = City()
-        obj6 = Amenity()
-        obj7 = Review()
-        models.storage.new(obj1)
-        models.storage.new(obj2)
-        models.storage.new(obj3)
-        models.storage.new(obj4)
-        models.storage.new(obj5)
-        models.storage.new(obj6)
-        models.storage.new(obj7) 
-        models.storage.save()
-        with open("file.json", "r") as file:
-            json_data = file.read()
-            self.assertIn("BaseModel." + obj1.id, json_data)
-            self.assertIn("User." + obj2.id, json_data)
-            self.assertIn("State." + obj3.id, json_data)
-            self.assertIn("Place." + obj4.id, json_data)
-            self.assertIn("City." + obj5.id, json_data)
-            self.assertIn("Amenity." + obj6.id, json_data)
-            self.assertIn("Review." + obj7.id, json_data)
+        obj = City()
+        self.storage_1.new(obj)
+        self.storage_1.save()
+        with open('file.json', 'r') as file:
+            data = json.load(file)
+            self.assertIn("City.{}".format(obj.id), data)
 
-    def test_save_with_arg(self):
-        with self.assertRaises(TypeError):
-            models.storage.save(None)
-
-    def test_reload(self):
-        x = BaseModel()
-        y = User()
-        z = State()
-        a = Place()
-        b = City()
-        c = Amenity()
-        d = Review()
-        models.storage.new(x)
-        models.storage.new(y)
-        models.storage.new(z)
-        models.storage.new(a)
-        models.storage.new(b)
-        models.storage.new(c)
-        models.storage.new(d)
-        models.storage.save()
-        models.storage.reload()
-        objs = FileStorage._FileStorage__objects
-        self.assertIn("BaseModel." + x.id, objs)
-        self.assertIn("User." + y.id, objs)
-        self.assertIn("State." + z.id, objs)
-        self.assertIn("Place." + a.id, objs)
-        self.assertIn("City." + b.id, objs)
-        self.assertIn("Amenity." + c.id, objs)
-        self.assertIn("Review." + d.id, objs)
-    
-    def test_reload_no_file(self):
-        self.assertRaises(FileNotFoundError, models.storage.reload())
-
-    def test_reload_with_arg(self):
-        with self.assertRaises(TypeError):
-            models.storage.reload(None)            
-
+    def test_reload(self):  
+        obj = City()
+        self.storage_1.new(obj)
+        self.storage_1.save()
+        self.storage_1.reload()     
+        objs = self.storage_1.all()
+        self.assertIn("City.{}".format(obj.id), objs)
 
 if __name__ == '__main__':
     unittest.main()    
