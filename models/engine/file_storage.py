b14887878ce61aad6d66ed6efd1a149b9a9b864c
jkarenzi <j.karenzi@alustudent.com>
2023-05-26 21:15:04 +0200

    changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -30,7 +30,6 @@ class FileStorage():
             json.dump(data, file)
 
     def reload(self):
-        from models.base_model import BaseModel
         try:
             with open(self.__file_path, 'r') as file:
                 data = json.load(file)
