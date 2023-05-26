cae7b0103782871cc86a5dc97aa518786367cc58
jkarenzi <j.karenzi@alustudent.com>
2023-05-26 18:55:43 +0200

    changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -46,3 +46,22 @@ class FileStorage():
                     self.__objects[key] = obj
         except FileNotFoundError:
             pass
+
+# Create an instance of FileStorage
+file_storage = FileStorage()
+
+# Reload previously saved objects from the JSON file
+file_storage.reload()
+
+# Create new instances of models
+user = User()
+state = State()
+city = City()
+
+# Add new objects to the file storage
+file_storage.new(user)
+file_storage.new(state)
+file_storage.new(city)
+
+# Save the objects to the JSON file
+file_storage.save()
