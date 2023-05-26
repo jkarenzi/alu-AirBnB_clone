183947393e78a753d2631dbcfec7221ca3feed85
jkarenzi <j.karenzi@alustudent.com>
2023-05-26 20:45:28 +0200

    changes

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The __init__ method for models directory"""
-from models.engine.file_storage import FileStorage
+
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -8,6 +8,7 @@ from models.city import City
 from models.amenity import Amenity
 from models.review import Review
 from models.place import Place
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
