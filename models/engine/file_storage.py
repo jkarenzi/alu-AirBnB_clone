5cdd13810a1c058005a8f680084c9348cadabbab
jkarenzi <j.karenzi@alustudent.com>
2023-05-26 20:29:06 +0200

    changes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,7 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+
 class FileStorage():
     """
     class that serializes instances to a JSON file and
